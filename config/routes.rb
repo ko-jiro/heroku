34032905292228596539619c78de9bd24412b47b
ko-jiro <1108239y@gmail.com>
2019-04-03 22:44:55 +0900

    確認テスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'topics/new'
   get 'sessions/new'
   # get 'users/new'     resources :usersの中に入ってるからいらないやつ
-  # post 'users/new', 'users#create'       メンタリング時に試しに入力したやつ
+  # post 'users/new', to: 'users#create'       メンタリング時に試しに入力したやつ
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#index"
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :comments
 end
